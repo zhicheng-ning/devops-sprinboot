8a41e6eac84fa53764d131deb15d031faebd8907
nzcer <ningzhicheng1215@qq.com>
2022-03-13 12:19:02 +0800

    update controller

--- src/main/java/com/example/devopssprinboot/controller/HelloController.java
+++ src/main/java/com/example/devopssprinboot/controller/HelloController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
-    @GetMapping("/hi")
+    @GetMapping("/world")
     public String sayHello() {
         return "hello, github action in springboot";
     }
