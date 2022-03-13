3adcb89d2e0b2cf7926795cca9fb95753c6808d9
nzcer <ningzhicheng1215@qq.com>
2022-03-13 12:56:12 +0800

    update yml

--- src/main/java/com/example/devopssprinboot/controller/HelloController.java
+++ src/main/java/com/example/devopssprinboot/controller/HelloController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
-    @GetMapping("/world")
+    @GetMapping("/hi")
     public String sayHello() {
         return "hello, github action in springboot";
     }
